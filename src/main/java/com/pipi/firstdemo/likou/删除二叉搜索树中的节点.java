4f178b28a2a2838d722fb06691a2a19f209033f2
like <1217414642@qq.com>
2021-08-26 11:03:48 +0800

    firstdemo 1

--- src/main/java/com/pipi/firstdemo/likou/删除二叉搜索树中的节点.java
+++ src/main/java/com/pipi/firstdemo/likou/删除二叉搜索树中的节点.java
@@ -4,11 +4,34 @@ package com.pipi.firstdemo.likou;
 import javax.swing.tree.TreeNode;
 
 public class 删除二叉搜索树中的节点 {
+    /**
+     * 4
+     * 2   6
+     * 5  7
+     */
+    public static void main(String[] args) {
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node7 = new TreeNode(7);
+        node6.left = node5;
+        node6.right = node7;
+        node4.right = node6;
+        node4.left = node2;
+      /*  TreeNode node = deleteNode(node4,6);
+        System.out.println(123);*/
+        successor(node6);
+        System.out.println(123);
+
+    }
 
     /**
-     * 后继节点
+     * 4
+     * 2   6
+     * 5  7
      */
-    public int successor(TreeNode root) {
+    public static int successor(TreeNode root) {
         root = root.right;
         while (root.left != null) root = root.left;
         return root.val;
@@ -17,32 +40,30 @@ public class 删除二叉搜索树中的节点 {
     /*
     One step left and then always right
     */
-    public int predecessor(TreeNode root) {
+    public static int predecessor(TreeNode root) {
         root = root.left;
         while (root.right != null) root = root.right;
         return root.val;
     }
 
-    public TreeNode deleteNode(TreeNode root, int key) {
+    /**
+     * 后继节点
+     */
+    public static TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) return null;
-
-        // delete from the right subtree
-        if (key > root.val) root.right = deleteNode(root.right, key);
-            // delete from the left subtree
-        else if (key < root.val) root.left = deleteNode(root.left, key);
-            // delete the current node
-        else {
-            // the node is a leaf
-            if (root.left == null && root.right == null) root = null;
-                // the node is not a leaf and has a right child
-            else if (root.right != null) {
+        if (key > root.val) {
+            deleteNode(root.right, key);
+        } else if (key < root.val) {
+            deleteNode(root.left, key);
+        } else {
+            if (root.right != null) {
                 root.val = successor(root);
                 root.right = deleteNode(root.right, root.val);
-            }
-            // the node is not a leaf, has no right child, and has a left child
-            else {
+            } else if (root.left != null) {
                 root.val = predecessor(root);
                 root.left = deleteNode(root.left, root.val);
+            } else {
+                root = null;
             }
         }
         return root;

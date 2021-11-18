3191d277d24a13769317ce2a383b0d9e69afed4a
keyLee520 <1217414642@qq.com>
2021-11-18 19:59:17 +0800

    #998 threadcommit

---
+++ src/main/java/com/pipi/firstdemo/jvm/Test1.java
@@ -0,0 +1,14 @@
+package com.pipi.firstdemo.jvm;
+
+public class Test1 {
+    public static final int num = 4;
+    public static int u = 1;
+
+    String dage;
+
+    public void gete() {
+        int x = 4;
+        int y = 5;
+        int i = x + y;
+    }
+}
